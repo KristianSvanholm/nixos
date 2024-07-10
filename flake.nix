c734f79f64cf4f0c9a248c436fae15cf7d096ba9
krs <kristianrorensvanholm@gmail.com>
2024-07-10 20:27:41 +0200

    restructure w/ flakes + home manager

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/default/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+      # Other hosts here
+    };
+  };
+}
