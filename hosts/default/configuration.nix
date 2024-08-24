42851d4ed63bc83ddb908e7e6b328ea0cb7c3fd2
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2024-08-24 18:36:19 +0200

    file browser

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -6,110 +6,89 @@
 
 {
 
-  # Activate flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes"];
+    # Activate flakes
+    nix.settings.experimental-features = [ "nix-command" "flakes"];
 
-  imports =
+    imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-      ../../modules/nixos/boot.nix
-      ../../modules/nixos/stylix.nix
-      ../../modules/nixos/bluetooth.nix
-      ../../modules/nixos/nvidia.nix
-      ../../modules/nixos/audio.nix
-      ../../modules/nixos/i3.nix
-      ../../modules/nixos/steam.nix
+	./hardware-configuration.nix
+	inputs.home-manager.nixosModules.default
+	../../modules/nixos/boot.nix
+	../../modules/nixos/stylix.nix
+	../../modules/nixos/bluetooth.nix
+	../../modules/nixos/nvidia.nix
+	../../modules/nixos/audio.nix
+	../../modules/nixos/i3.nix
+	../../modules/nixos/steam.nix
+	../../modules/nixos/thunar.nix
     ];
 
-  networking.hostName = "nixos"; # Define your hostname.
+    networking.hostName = "nixos"; # Define your hostname.
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Oslo";
+    # Set your time zone.
+    time.timeZone = "Europe/Oslo";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "no";
-    variant = "";
-  };
+    # Configure keymap in X11
+    services.xserver.xkb = {
+	layout = "no";
+	variant = "";
+    };
 
-  # vpn
-  services.mullvad-vpn.enable = true;
+    # vpn
+    services.mullvad-vpn.enable = true;
 
-  # Configure console keymap
-  console.keyMap = "no";
+    # Configure console keymap
+    console.keyMap = "no";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.krs = {
-    isNormalUser = true;
-    description = "krs";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.krs = {
+	isNormalUser = true;
+	extraGroups = [ "networkmanager" "wheel" ];
+    };
 
-  home-manager = {
+    home-manager = {
   	useGlobalPkgs = true;
 	extraSpecialArgs= { inherit inputs; };
 	users = {
-	  "krs" = import ./home.nix;
+	    "krs" = import ./home.nix;
 	};
-  };
+    };
 
-  # Install firefox.
-  programs.firefox.enable = true;
+    # Install firefox.
+    programs.firefox.enable = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. 
-  environment.systemPackages = with pkgs; [
-    git
-    htop
-    nvtopPackages.full
-    neofetch
-    go
-    binutils
-    home-manager
-    gcc
-    ripgrep
-    fd
-    mullvad-vpn
-  ];
-
-#  programs.neovim = {
-#	enable = true;
-#	defaultEditor = true;
-#	withNodeJs = true;
-#	withPython3 = true;
- # };
-
-  fileSystems."/jellyfin" = {
-    device = "192.168.3.112:/jellyfin";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "user"];
-  };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+    # List packages installed in system profile. 
+    environment.systemPackages = with pkgs; [
+	git
+	htop
+	nvtopPackages.full
+	neofetch
+	go
+	binutils
+	home-manager
+	gcc
+	ripgrep
+	fd
+	mullvad-vpn
+    ];
+  
+    fileSystems."/jellyfin" = {
+	device = "192.168.3.112:/jellyfin";
+	fsType = "nfs";
+	options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "user"];
+    };
+ 
+    system.stateVersion = "24.05"; # Don't touch
 }
