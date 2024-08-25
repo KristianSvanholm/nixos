0a9da79fa22c29c1be8285b17786763868394a57
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2024-08-25 15:22:20 +0200

    typo

--- modules/nixos/qbittorrent-nox.nix
+++ modules/nixos/qbittorrent-nox.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-    environment.systemPackages = with pkgs; [ qbittorrent-now ];
+    environment.systemPackages = with pkgs; [ qbittorrent-nox ];
 
     systemd = {
 	packages = [pkgs.qbittorrent-nox];
