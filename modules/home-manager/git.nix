ea41413cab0b006ea1fc5e491a9b158b8119226a
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2024-08-25 14:12:30 +0200

    More git updates

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -2,11 +2,14 @@
 
 {
     # git
-    home.packages = [pkgs.git];
-    programs.git.enable = true;
-    programs.git.userName = "KristianSvanholm";
-    programs.git.userEmail = "KristianRorenSvanholm@gmail.com";
-    programs.git.extraConfig = {
-        init.defaultBranch = "main";
+    programs.git = {
+	enable = true;
+	userName = "KristianSvanholm";
+	userEmail = "KristianRorenSvanholm@gmail.com";
+	extraConfig = {
+	    init.defaultBranch = "main";
+	    push.autoSetupRemote = true;
+	    pull.rebase = false;
+	};
     };
 }
