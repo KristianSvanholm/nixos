6b1675c1d7a495fd062dcc429be6ca4bf4c582ff
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2024-11-22 16:51:32 +0100

    Stylix worse option for this

--- modules/home/btop.nix
+++ modules/home/btop.nix
@@ -1,8 +1,11 @@
 { ... }:
 {
+    stylix.targets.btop.enable = false;
+
     programs.btop = {
 	enable = true;
 	settings = {
+	    color_theme = "gruvbox_dark_v2";
 	    theme_background = false;
 	    rounded_corners = true;
 	    graph_symbol = "braille";
