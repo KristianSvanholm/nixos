b2ae79c61a520924a5f7a5bd3a22c1699612f7e2
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2024-11-22 02:18:20 +0100

    better waybar

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -1,33 +1,75 @@
 { ... }:
 {
+
+    stylix.targets.waybar.enable = false;
     programs.waybar = {
 	enable = true;
-	settings = [{
-	    /*mainbar = {
-		layer = "bottom";
-		position = "bottom"; 
-		margin-bottom = -1;
-	    };*/
-	    modules-left = [ "hyprland/workspaces" ]; 
-	    modules-center = [ "clock" ];
-	    modules-right = [ "pulseaudio" "bluetooth" "cpu" "memory" "temperature" ];
-	    pulseaudio = {
-		format = "vol: {volume}%";
-		on-click = "pavucontrol";
+	settings = {
+	    mainBar = {
+
+		modules-left = [ "hyprland/workspaces" ]; 
+		modules-center = [ "clock" ];
+		modules-right = [ "pulseaudio" "bluetooth" "cpu" "memory" "temperature" "clock#date" ];
+
+		"hyprland/workspaces" = {
+
+		};
+
+		pulseaudio = {
+		    format = "vol: {volume}%";
+		    on-click = "pavucontrol";
+		};
+		bluetooth = {
+		    on-click = "blueman-manager";
+		};
+		cpu = {
+		    format = "cpu: {usage}%";
+		};
+		memory = {
+		   format = "mem: {percentage}%";
+		};
+		clock = {
+		    format = "{:%H:%M}";
+		};
+		"clock#date" = {
+		    format = "{:%d.%m.%Y}"; 
+		};
 	    };
-	    bluetooth = {
-		on-click = "blueman-manager";
-	    };
-	    cpu = {
-		format = "cpu: {usage}%";
-	    };
-	    memory = {
-		format = "mem: {percentage}%";
-	    };
-	    clock = {
-		format = "{:%d.%m.%Y   %H:%M}";
-	    };
-	}];
+	};
+	style = ''
+	
+	* {
+	    border: none;
+	    border-radius: 0;
+	    min-height: 0;
+	    margin: 0px;
+	}
+
+	#window {
+	    color: #e4e4e4;
+	}
+
+	#workspaces {
+	    padding: 0px;
+	    margin: 0px;
+	}
+
+	#workspaces button {
+	    padding: 0 2px;
+	    margin: 0px;
+	    font-weight: bold;
+	}
+	
+	#workspaces button.active {
+	    background: #6e6e6e;
+	}
+
+	.modules-right * {
+	    padding-left: 3px;
+	    padding-right: 3px;
+	}
+
+	'';
     };
 
 }
