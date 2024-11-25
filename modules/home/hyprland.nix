218618244db258ac5d60a8531779e4818cf224c4
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2024-11-25 13:30:10 +0100

    Switched the screen inputs to fix cursor starter position

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -64,21 +64,21 @@
 	    exec-once = [ "waybar" "hyprpaper" ];
 
 	    monitor = [
-		"DP-2, 2560x1440@144, 0x0, 1"
-		"DP-1, 1920x1080@144, 2560x0, 1"
+		"DP-1, 2560x1440@144, 0x0, 1"
+		"DP-2, 1920x1080@144, 2560x0, 1"
 	    ];
 
 	    workspace = [
-		"1,monitor:DP-2"
-		"2,monitor:DP-1"
-		"3,monitor:DP-2"
-		"4,monitor:DP-1"
-		"5,monitor:DP-2"
-		"6,monitor:DP-1"
-		"7,monitor:DP-2"
-		"8,monitor:DP-1"
-		"9,monitor:DP-2"
-		"0,monitor:DP-1"
+		"1,monitor:DP-1"
+		"2,monitor:DP-2"
+		"3,monitor:DP-1"
+		"4,monitor:DP-2"
+		"5,monitor:DP-1"
+		"6,monitor:DP-2"
+		"7,monitor:DP-1"
+		"8,monitor:DP-2"
+		"9,monitor:DP-1"
+		"0,monitor:DP-2"
 	    ];
 
 	    bind = [
