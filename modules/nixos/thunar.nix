6b6e826649d869ceffbc5b9549c5798fb1a5a137
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2025-02-03 12:43:25 +0100

    plugins

--- modules/nixos/thunar.nix
+++ modules/nixos/thunar.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
-    programs.thunar.enable = true;
+    programs.thunar = {
+        enable = true;
+        plugins = with pkgs.xfce; [
+            thunar-archive-plugin
+            thunar-volman
+        ];
+    };
     programs.xfconf.enable = true;
     services.tumbler.enable = true;
     services.gvfs.enable = true;
