7de172b62187de3b291a9298e70b2a80a5904079
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2025-02-03 12:04:20 +0100

    guiding comment

--- modules/home/modrinth.nix
+++ modules/home/modrinth.nix
@@ -2,6 +2,7 @@
 {
     home.packages = with pkgs; [modrinth-app];
 
+    # Files located in ~/.nix-profile/share/applications/
     xdg.desktopEntries."Modrinth App" = {
         name = "Modrinth App";
         exec = "env WEBKIT_DISABLE_DMABUF_RENDERER=1 GDK_BACKEND=x11 ModrinthApp";
