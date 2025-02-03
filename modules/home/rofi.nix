2a443344218528083c28c763c5adf1d3942f90d0
Kristian Svanholm <kristianrorensvanholm@gmail.com>
2025-02-03 12:32:37 +0100

    rofi calc

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 {
     
-    home.packages = with pkgs; [rofi-power-menu];
-
     programs.rofi = {
 	enable = true;
 	package = pkgs.rofi-wayland; # This works on both xorg and wayland :D
@@ -14,11 +12,19 @@
 	    drun-display-format = "{icon} {name}";
 	    window-display-format = "{icon} {name}";
 	};
+        plugins = with pkgs; [ rofi-power-menu rofi-calc ];
     };
 
-    xdg.desktopEntries."rofi-power" = {
-        name = "Power options";
-        icon = "Rofi";
-        exec = "rofi -show power-menu -modi \"power-menu:rofi-power-menu --choices=shutdown/reboot\"";
+    xdg.desktopEntries = {
+        "rofi-power" = {
+            name = "Power options";
+            icon = "Rofi";
+            exec = "rofi -show power-menu -modi \"power-menu:rofi-power-menu --choices=shutdown/reboot\"";
+        };
+        "rofi-calc" = {
+            name = "Calc";
+            icon = "Rofi";
+            exec = "rofi -show calc -modi calc -no-show-match -no-sort";
+        };
     };
 }
