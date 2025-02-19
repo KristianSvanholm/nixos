e967be4b59999d202611c1a4a82a5147126b1a74
kristian <kristianrorensvanholm@gmail.com>
2025-02-19 11:26:51 +0100

    signal cask

--- modules/home/nvf.nix
+++ modules/home/nvf.nix
@@ -1,23 +1,47 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
 
-    imports = [ inputs.nvf.homeManagerModules.default ];
+    imports = [ 
+        inputs.nvf.homeManagerModules.default
+        ./latex.nix 
+    ];
 
     programs.nvf = {
         enable = true;
 	  settings = {
 	    vim = {
 
+                globals = {
+                    maplocalleader = " ";
+                };
+
 		theme = {
 		    enable = true;
 		    name = "gruvbox";
 		    style = "dark";
 		};
-  
+            
+
                 # Plugins
 		statusline.lualine.enable = true;
 		autocomplete.nvim-cmp.enable = true;
                 git.gitsigns.enable = true;
+
+		extraPlugins = with pkgs.vimPlugins; {
+		    vimtex = {
+			package = vimtex;
+                        #setup = "require('vimtex').setup {}";
+			after =''
+                            filetype plugin indent on
+                            syntax enable
+
+                            let g:vimtex_view_method = 'zathura'
+
+                            let g:vimtex_compiler_method = 'latexmk'
+
+                        '';
+		    };
+		};
 
                 telescope = {
                     enable = true;
