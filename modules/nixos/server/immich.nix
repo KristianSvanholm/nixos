8bf0ed530f04b675f07d5786e5b543a70baa8f27
KristianSvanholm <KristianRorenSvanholm@gmail.com>
2025-03-04 02:32:56 +0100

    immich transcode

--- modules/nixos/server/immich.nix
+++ modules/nixos/server/immich.nix
@@ -6,6 +6,9 @@
 	openFirewall = true;
 	mediaLocation = "/mnt/hdd/immich";
 	host = "192.168.3.112";
+	#accelerationDevices = null;
     };
+
+    users.users.immich.extraGroups = [ "video" "render" ];
 
 }
