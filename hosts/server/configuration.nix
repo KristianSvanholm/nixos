674e51004d73e966a7e04d326389c84ab2f5f0db
KristianSvanholm <KristianRorenSvanholm@gmail.com>
2025-03-06 04:49:06 +0100

    adguard + nginx move ports

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -13,6 +13,7 @@
 	../../modules/nixos/server/intel.nix
 	../../modules/nixos/server/nginx.nix
 	../../modules/nixos/server/yarr.nix
+	../../modules/nixos/server/adguard.nix
 	../../modules/nixos/server/immich.nix
 	#../../modules/nixos/server/minecraft/minecraft.nix
     ];
@@ -28,7 +29,6 @@
 	firewall = {
 	    enable = true;
 	    allowedTCPPorts = [
-		80 81 443 # nginx
 		22 # SSH
 		111 2049 4000 4001 4002 20048 # NFS
 		6969 # Qbittorrent-nox
---
+++ modules/nixos/server/adguard.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+
+    services.adguardhome = {
+	enable = true;
+	openFirewall = true;
+	host = "0.0.0.0";
+    };
+
+    networking.firewall = {
+	allowedTCPPorts = [ 53 1616 ];
+	allowedUDPPorts = [ 53 ];
+    };
+
+}
