1eeecb50a3270f4d132736edd1ad34f12b786ffe
KristianSvanholm <KristianRorenSvanholm@gmail.com>
2025-04-29 13:20:29 +0200

    git shorthands

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -10,6 +10,10 @@
 	    init.defaultBranch = "main";
 	    push.autoSetupRemote = true;
 	    pull.rebase = false;
+            url = {
+                "https://github.com/".insteadOf = ["gh:"];
+                "git@github.com:kristiansvanholm/".insteadOf = ["me:"];
+            };
 	};
     };
 }
