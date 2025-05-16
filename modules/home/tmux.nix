30c5b2af36a4334266f1246da774aa6d31737ced
kristian <kristianrorensvanholm@gmail.com>
2025-05-16 19:11:29 +0200

    intial sesh setup

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -6,14 +6,32 @@
     shortcut = "a";
     newSession = true;
     clock24 = true;
+    baseIndex = 1;
     extraConfig = ''
       bind -r h select-pane -L
       bind -r j select-pane -D
       bind -r k select-pane -U
       bind -r l select-pane -R
 
-      bind w split-window -h
+      bind v split-window -h
       bind s split-window -v
+
+      set-option -g status-position top
+      set-option -g status-justify centre
+
+      set -g detach-on-destroy off # Don't exit from tmux when closing a session
     '';
+  };
+
+  programs.sesh = {
+    enable = true;
+    tmuxKey = "space";
+    settings = {
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    tmux.enableShellIntegration = true;
   };
 }
