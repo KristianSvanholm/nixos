8587f405f5716647b239c57d510f66666457b7eb
KristianSvanholm <KristianRorenSvanholm@gmail.com>
2025-05-21 23:45:09 +0200

    update mini host

--- hosts/mini/configuration.nix
+++ hosts/mini/configuration.nix
@@ -1,80 +1,82 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ pkgs, inputs, username, home, ... }:
-
 {
+  pkgs,
+  inputs,
+  username,
+  home,
+  ...
+}: {
+  # Activate flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
-    # Activate flakes
-    nix.settings.experimental-features = [ "nix-command" "flakes"];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+    ../../modules/nixos/boot.nix
+    ../../modules/nixos/localization.nix
+    ../../modules/nixos/stylix.nix
+    ../../modules/nixos/bluetooth.nix
+    ../../modules/nixos/audio.nix
+    ../../modules/nixos/hyprland.nix
+    ../../modules/nixos/ly.nix
+    ../../modules/nixos/1pass.nix
+    ../../modules/nixos/r.nix
+    ../../modules/nixos/spicetify.nix
+  ];
 
-    imports =
-    [ # Include the results of the hardware scan.
-	./hardware-configuration.nix
-	inputs.home-manager.nixosModules.default
-	../../modules/nixos/boot.nix
-	../../modules/nixos/localization.nix
-	../../modules/nixos/stylix.nix
-	../../modules/nixos/bluetooth.nix
-	../../modules/nixos/audio.nix
-	../../modules/nixos/hyprland.nix
-	../../modules/nixos/ly.nix
-	../../modules/nixos/1pass.nix
-        ../../modules/nixos/r.nix
-        ../../modules/nixos/spicetify.nix
-    ];
+  networking = {
+    hostName = "nixos";
+    nameservers = ["192.168.3.112"];
+    firewall.enable = true;
+    enableIPv6 = true;
+    networkmanager.enable = true;
+  };
 
-    networking = {
-	hostName = "nixos";
-        nameservers = ["192.168.3.112"];
-	firewall.enable = true;
-	enableIPv6 = true;
-	networkmanager.enable = true;
+  programs = {
+    firefox.enable = false;
+    zsh.enable = true;
+  };
+
+  services = {
+    mullvad-vpn.enable = true;
+    printing.enable = true;
+  };
+
+  users = {
+    users.${username} = {
+      isNormalUser = true;
+      extraGroups = ["networkmanager" "wheel" "libvirtd"];
     };
 
-    programs = {
-	firefox.enable = false;
-	zsh.enable = true;
-    };
-    
-    services = {
-	mullvad-vpn.enable = true;
-	printing.enable = true;
-    };
+    defaultUserShell = pkgs.zsh;
+  };
 
+  home-manager = {
+    useGlobalPkgs = true;
+    extraSpecialArgs = {inherit inputs username home;};
     users = {
-	users.${username} = {
-	    isNormalUser = true;
-	    extraGroups = [ "networkmanager" "wheel" "libvirtd"];
-	};
-
-	defaultUserShell = pkgs.zsh;
+      ${username} = import ./home.nix;
     };
+  };
 
-    home-manager = {
-  	useGlobalPkgs = true;
-	extraSpecialArgs= { inherit inputs username home; };
-	users = {
-	    ${username} = import ./home.nix;
-	};
-    };
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    nvtopPackages.full
+    go
+    binutils
+    gcc
+    zulu17
+    maven
+    nix-prefetch-github
+    inputs.nur-packages.packages.${pkgs.hostPlatform.system}.zz
+    inputs.nur-packages.packages.${pkgs.hostPlatform.system}.p2ptui
+  ];
 
-    # List packages installed in system profile. 
-    environment.systemPackages = with pkgs; [
-	nvtopPackages.full
-	go
-	binutils
-	gcc
-	zulu17
-	maven
-	nix-prefetch-github
-        inputs.nur-packages.packages.${pkgs.hostPlatform.system}.zz
-        inputs.nur-packages.packages.${pkgs.hostPlatform.system}.p2ptui
-    ];
-  
-    system.stateVersion = "24.05"; # Don't touch
+  system.stateVersion = "24.05"; # Don't touch
 }
