63be6a8620dec57ecd1d766a63ac77e7637a8f42
KristianSvanholm <kristian-roren.svanholm@gjensidige.no>
2025-08-21 13:25:53 +0000

    work stuff

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -1,16 +1,30 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }: {
-  imports = [../configuration.nix];
+  imports = [
+    inputs.nixos-wsl.nixosModules.default
+    ../../modules/nixos/stylix.nix
+    ../configuration.nix
+  ];
 
   wsl.enable = true;
   wsl.defaultUser = config.user.name;
 
   home-manager.users.${config.user.name} = import ./home.nix;
 
+  virtualisation.docker = {
+    enable = false;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
+    docker-compose
     binutils
     gcc
     maven
