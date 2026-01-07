1963c03a4167b26390d0c9ca60120f91678da6c9
KristianSvanholm <kristian-roren.svanholm@gjensidige.no>
2026-01-07 14:07:32 +0000

    add helix

---
+++ modules/home/helix.nix
@@ -0,0 +1,72 @@
+{ pkgs, ... }:
+{
+  programs.helix = {
+    enable = true;
+    extraPackages = with pkgs; [
+      nil
+      rust-analyzer
+      gopls
+      jdt-language-server
+      nixfmt-rfc-style
+    ];
+    settings = {
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+        true-color = true;
+        indent-guides.render = true;
+      };
+    };
+    languages = {
+      language-server = {
+        nil = {
+          command = "${pkgs.nil}/bin/nil";
+        };
+
+        gopls = {
+          command = "${pkgs.gopls}/bin/gopls";
+        };
+
+        rust-analyzer = {
+          command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+        };
+
+        jdtls = {
+          command = "${pkgs.jdt-language-server}/bin/jdtls";
+        };
+      };
+
+      language = [
+        # Nix
+        {
+          name = "nix";
+          auto-format = true;
+          formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+          language-servers = [ "nil" ];
+        }
+
+        # Go
+        {
+          name = "go";
+          auto-format = true;
+          formatter.command = "${pkgs.go}/bin/gofmt";
+          language-servers = [ "gopls" ];
+        }
+
+        # Rust
+        {
+          name = "rust";
+          auto-format = true;
+          language-servers = [ "rust-analyzer" ];
+        }
+
+        # Java
+        {
+          name = "java";
+          auto-format = true;
+          language-servers = [ "jdtls" ];
+        }
+      ];
+    };
+  };
+}
