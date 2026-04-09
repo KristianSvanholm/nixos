380518357e90250cbe37fd7808d0d340e68d8b23
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-04-09 13:11:36 +0200

    windows boot

--- modules/nixos/limine.nix
+++ modules/nixos/limine.nix
@@ -1,6 +1,6 @@
 {...}: {
   boot.consoleLogLevel = 0;
-  boot.kernelParams = ["quiet" "udev.log_level=3"];
+  boot.kernelParams = ["quiet" "udev.log_level=3" "usbcore.autosuspend=-1"];
   boot.initrd.verbose = false;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
@@ -8,5 +8,10 @@
     enable = true;
     efiSupport = true;
     maxGenerations = 10;
+    extraEntries = ''
+      /Windows
+          protocol: efi
+          path: guid(64aceaf0-5244-4fde-91d8-e3883894613e):/EFI/Microsoft/Boot/bootmgfw.efi
+    '';
   };
 }
