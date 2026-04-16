011013fd479fbc746ca60f2c05f5ddaea13f54b1
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-04-16 13:26:26 +0200

    proton-ge

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -1,10 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   #steam
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in fw for steam remote play
     dedicatedServer.openFirewall = true; # Open ports in fw for Source dedicated server
     localNetworkGameTransfers.openFirewall = true; # Open ports in fw for steam local network game transfers
+    extraCompatPackages = [pkgs.proton-ge-bin];
   };
 
   programs.gamemode.enable = true;
