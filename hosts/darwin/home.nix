ce0b147a223afa9fe5ac466e5a80126bbf7a04ff
KristianSvanholm <kristian-roren.svanholm@gjensidige.no>
2026-04-24 14:16:14 +0200

    fix ssh conf

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -19,7 +19,7 @@
       enableDefaultConfig = false;
       matchBlocks."*" = {
         extraOptions = {
-          IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+          IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
         };
       };
     };
