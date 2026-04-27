17ce15b2e8dbf47b2c4bc093ca946c07cb5a3c29
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-04-27 11:25:22 +0200

    adguard in k3s

--- modules/nixos/homelab/k3s.nix
+++ modules/nixos/homelab/k3s.nix
@@ -5,13 +5,17 @@
   networking.firewall = {
     allowedTCPPorts = [
       6443 # k3s
+      53 # dns
       80 # web
+      3000 # adguard setup
       443 # websec
       8080 # webext
       8443 # websecext
       25565 # minecraft
     ];
     allowedUDPPorts = [
+      53 # dns
+      443 # web
       51820 # wireguard
       24454 # minecraft-vc
       8472 # flannel: require if using multi node for inter-node networking
