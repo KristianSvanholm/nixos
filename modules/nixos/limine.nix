48f8d51e69e8ef4a2297e3fb4526b1bf26fbc8c2
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-07-07 20:32:43 +0200

    secure boot

--- modules/nixos/limine.nix
+++ modules/nixos/limine.nix
@@ -1,17 +1,29 @@
-{...}: {
-  boot.consoleLogLevel = 0;
-  boot.kernelParams = ["quiet" "udev.log_level=3" "usbcore.autosuspend=-1"];
-  boot.initrd.verbose = false;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.limine = {
-    enable = true;
-    efiSupport = true;
-    maxGenerations = 10;
-    extraEntries = ''
-      /Windows
-          protocol: efi
-          path: guid(64aceaf0-5244-4fde-91d8-e3883894613e):/EFI/Microsoft/Boot/bootmgfw.efi
-    '';
+{pkgs, ...}: {
+  environment.systemPackages = [pkgs.sbctl];
+  stylix.targets.plymouth.enable = false;
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "hexagon";
+      themePackages = [pkgs.adi1090x-plymouth-themes];
+    };
+    consoleLogLevel = 0;
+    kernelParams = ["quiet" "udev.log_level=3" "usbcore.autosuspend=-1"];
+    initrd.verbose = false;
+    loader = {
+      efi.canTouchEfiVariables = true;
+      efi.efiSysMountPoint = "/boot";
+      limine = {
+        enable = true;
+        secureBoot.enable = true;
+        efiSupport = true;
+        maxGenerations = 10;
+        extraEntries = ''
+          /Windows
+              protocol: efi
+              path: guid(64aceaf0-5244-4fde-91d8-e3883894613e):/EFI/Microsoft/Boot/bootmgfw.efi
+        '';
+      };
+    };
   };
 }
