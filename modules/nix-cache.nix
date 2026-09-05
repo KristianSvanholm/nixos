b45dee94f6b1fea3b0371c33758af13307ee740b
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-09-06 01:44:41 +0200

    add cache path to substituters

--- modules/nix-cache.nix
+++ modules/nix-cache.nix
@@ -2,7 +2,7 @@
   nix.settings = {
     connect-timeout = 5;
     fallback = true;
-    substituters = ["http://192.168.3.112:30800"];
+    substituters = ["http://192.168.3.112:30800/homelab"];
     trusted-public-keys = ["homelab:SOrR7Lbq4eAhpglgP6XjGRzS9j5M05KT+ewCH16rwyo="];
   };
 }
