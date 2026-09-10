e80e3a8427093bd9bfc8e5483ab702b1de173147
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-09-10 19:35:07 +0200

    turn off hfp/hsp for bluetooth devices

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -7,6 +7,16 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+
+    # Disable the Bluetooth handsfree/headset (HFP/HSP) roles so headsets
+    # (e.g. Sony XM3) only expose high-quality A2DP output and never a
+    # low-quality mic, leaving the standalone microphone as the default input.
+    wireplumber.extraConfig."51-disable-bluetooth-hfp" = {
+      "monitor.bluez.properties" = {
+        "bluez5.roles" = ["a2dp_sink" "a2dp_source"];
+        "bluez5.autoswitch-profile" = false;
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
