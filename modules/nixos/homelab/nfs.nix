d378f7d16eb8f81675d7d30953fabc6e1f60c19c
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-09-21 18:57:45 +0200

    nfs mount for longhorn backups + declarative hdd dirs

--- modules/nixos/homelab/nfs.nix
+++ modules/nixos/homelab/nfs.nix
@@ -6,10 +6,23 @@
     lockdPort = 4001;
     mountdPort = 4002;
 
+    # longhorn-manager is not host-networked, so it mounts the backupstore from
+    # the pod network. Only that subtree is exported to it.
     exports = ''
       /mnt/hdd 192.168.3.0/24(rw,sync,no_subtree_check,no_root_squash,insecure)
+      /mnt/hdd/backups/longhorn 192.168.3.0/24(rw,sync,no_subtree_check,no_root_squash,insecure) 10.42.0.0/16(rw,sync,no_subtree_check,no_root_squash,insecure)
     '';
   };
+
+  # backup roots; jotta-cli backs /mnt/hdd/backups up offsite
+  systemd.tmpfiles.rules = [
+    "d /mnt/hdd/backups 0755 root root -"
+    "d /mnt/hdd/backups/longhorn 0755 root root -"
+    "d /mnt/hdd/backups/postgres 0755 root root -"
+    # written by the pg-dump jobs, which run as the namespace's own user
+    "d /mnt/hdd/backups/postgres/attic 2770 attic attic -"
+    "d /mnt/hdd/backups/postgres/immich 2770 immich immich -"
+  ];
 
   networking.firewall = {
     allowedTCPPorts = [
