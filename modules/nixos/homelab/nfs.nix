19446178a82424607df81f71fa71a740c85ae698
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-09-21 21:32:35 +0200

    nfs hardening

--- modules/nixos/homelab/nfs.nix
+++ modules/nixos/homelab/nfs.nix
@@ -1,31 +1,36 @@
-{config, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   owner = config.users.users.${config.user.name};
-  lan = "192.168.3.0/24";
+  # the only nfs clients; both reserved in the router
+  nodes = ["192.168.3.112" "192.168.3.91"];
   pods = "10.42.0.0/16";
-  base = "rw,sync,no_subtree_check,insecure";
+  base = "rw,sync,no_subtree_check";
+  exportTo = clients: opts: lib.concatMapStringsSep " " (c: "${c}(${opts})") clients;
 
   # no_root_squash because linuxserver entrypoints run as root before dropping
   # to PUID
-  app = fsid: "${lan}(${base},no_root_squash,fsid=${toString fsid})";
+  app = fsid: exportTo nodes "${base},no_root_squash,fsid=${toString fsid}";
 
   # squashed to the local user so jottad can read what longhorn writes as root
   squash = "${base},all_squash,anonuid=${toString owner.uid},anongid=${toString config.users.groups.${owner.group}.gid},fsid=5";
 in {
   services.nfs.server = {
     enable = true;
-    # pinned so the firewall rules below are enough
-    statdPort = 4000;
-    lockdPort = 4001;
-    mountdPort = 4002;
 
     exports = ''
       /mnt/hdd/jelly ${app 1}
       /mnt/hdd/immich ${app 2}
       /mnt/hdd/attic ${app 3}
       /mnt/hdd/test-media ${app 4}
-      /mnt/hdd/backups ${lan}(${squash}) ${pods}(${squash})
+      /mnt/hdd/backups ${exportTo (nodes ++ [pods]) squash}
     '';
   };
+
+  # every client is v4; v3 would also drag in rpcbind, statd and lockd
+  services.nfs.settings.nfsd.vers3 = false;
 
   systemd.tmpfiles.rules = let
     o = "${config.user.name} ${owner.group}";
@@ -39,14 +44,5 @@ in {
     "d /mnt/hdd/backups/postgres/immich 0755 ${o} -"
   ];
 
-  networking.firewall = {
-    allowedTCPPorts = [
-      111 # rpcbind
-      2049 # nfs
-      4000 # statd
-      4001 # lockd
-      4002 # mountd
-    ];
-    allowedUDPPorts = [111 2049 4000 4001 4002];
-  };
+  networking.firewall.allowedTCPPorts = [2049];
 }
