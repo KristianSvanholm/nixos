9a0c3f014f62d21b9af4049a3f10776196d1fc08
KristianSvanholm <kristianrorensvanholm@gmail.com>
2026-09-23 15:36:59 +0200

    add secondary authorized key

--- modules/nixos/homelab/ssh.nix
+++ modules/nixos/homelab/ssh.nix
@@ -11,5 +11,8 @@
     openFirewall = true; # 22
     settings.PasswordAuthentication = false;
   };
-  users.users.${config.user.name}.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOK8RufgRPvoPDRovOxteHN5ZEAXiJpXwJfVQuZsVtmj"];
+  users.users.${config.user.name}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOK8RufgRPvoPDRovOxteHN5ZEAXiJpXwJfVQuZsVtmj"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGYYv7IMr0kycn5IsftQV/y6+WdCr6SNf6CeB2oaP2zQ termix access key"
+  ];
 }
